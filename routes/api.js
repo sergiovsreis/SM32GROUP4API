d50f798b7b4df8b36127a197723b3ad518456a7b
Mike <mikevoermans.voermans@student.fontys.nl>
2016-05-19 14:39:53 +0200

    Api JS Fix

--- routes/api.js
+++ routes/api.js
@@ -8,12 +8,23 @@ router.get('/', function(req, res, next) {
 
 var memberApi = require('./member/index');
 var locationApi = require('./location/index');
+var groupApi = require('./group/index');
+var attendanceApi = require('./attendance/index');
 
+//Group API
+router.post('/addMember', groupApi.update);
+router.post('/getGroupMember', groupApi.get);
+router.post('/group', groupApi.create);
 
+//Member API
 router.post('/group', memberApi.create);
 router.post('/member', memberApi.create);
+
+//Location API
 router.post('/location', locationApi.create);
 
+//Attendance API
+router.post('/attendance', attendanceApi.create);
 
 router.post('/member/login', memberApi.login);
 router.get('/member', memberApi.requireUser, function(req,res, next) {
