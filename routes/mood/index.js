21a332d0843bd6b1afa2d84aff155ebb46f168c0
fhict <fhict>
2016-06-03 11:59:28 +0200

    mood pictures insert

--- routes/mood/index.js
+++ routes/mood/index.js
@@ -1,10 +1,15 @@
 /**
  * Created by Sergio Reis on 29-04-16.
  */
-
+var multer  = require('multer');
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
 var moodCreate = require('./create');
+
+module.exports.upload = upload;
 
 
 module.exports.create = moodCreate.create;
+
 
 
